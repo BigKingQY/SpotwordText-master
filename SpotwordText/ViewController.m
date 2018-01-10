f1faa196cdf2845e44528458f79b24fd042c27b3
覃运 <641648019@qq.com>
2018-01-10 14:09:29 +0800

    Initial Commit

---
+++ SpotwordText/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  SpotwordText
+//
+//  Created by Administrator on 2018/1/10.
+//  Copyright © 2018年 Administrator. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"A写了一段代码");
+    NSLog(@"B写了一段代码");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
